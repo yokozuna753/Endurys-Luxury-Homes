f630af8fc20694dbb7cb3110b19b2e8be97618b8
yokozuna753 <brian894526@gmail.com>
2025-09-11 10:49:09 -0400

    removed image bg-black color covering images in dark mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import HomePage from './LandingPage/HomePage.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import HomePage from "./LandingPage/HomePage.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HomePage />
-  </StrictMode>,
-)
+  </StrictMode>
+);
