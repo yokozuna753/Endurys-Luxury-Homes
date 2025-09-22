6231f96b975f8da601b092e540cddbcde55cb734
yokozuna753 <brian894526@gmail.com>
2025-09-22 15:18:22 -0400

    Fix Netlify config with redirects

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
+  },
+  build: {
+    outDir: "dist", // <-- force Vite to output directly into dist/
   },
 });
