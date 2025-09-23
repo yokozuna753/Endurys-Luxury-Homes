daca8151d73d8c9cb1761f7181f1e77989884533
yokozuna753 <brian894526@gmail.com>
2025-09-23 18:06:23 -0400

    removed unused imports

--- frontend/src/components/ShowcaseCustomHomes/ShowcaseCustomHomes.jsx
+++ frontend/src/components/ShowcaseCustomHomes/ShowcaseCustomHomes.jsx
@@ -1,15 +1,9 @@
-import { Navigate, useParams } from "react-router-dom";
-import { propertiesObj } from "../Properties/PropertiesArray/properties.js";
 import TopMedia from "./TopMedia.jsx";
-import Description from "./Description.jsx";
-import Amenities from "./ShowcaseProperties.jsx";
-import Pictures from "./Pictures.jsx";
-import Maps from "./Maps.jsx";
 import ContactForm from "../ContactForm/ContactForm.jsx";
 import Footer from "../Footer/Footer.jsx";
 import ShowcaseProperties from "./ShowcaseProperties.jsx";
 
-export default function CustomHomes() {
+export default function ShowcaseCustomHomes() {
   //   ✅ If invalid, route to your NotFound ("*") page
 
   return (
