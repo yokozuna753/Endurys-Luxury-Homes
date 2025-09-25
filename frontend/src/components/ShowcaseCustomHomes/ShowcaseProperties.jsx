5eaa16a30dd2e8b2975871474bf96b07c91a0166
yokozuna753 <brian894526@gmail.com>
2025-09-25 15:18:45 -0400

    changed paths on custom homes/showcase & custom homes pages'

--- frontend/src/components/ShowcaseCustomHomes/ShowcaseProperties.jsx
+++ frontend/src/components/ShowcaseCustomHomes/ShowcaseProperties.jsx
@@ -8,7 +8,7 @@ export default function ShowcaseProperties() {
 
   const navigate = useNavigate();
 
-  const properties = [...propertiesObj.miamiDade, ...propertiesObj.pinecrest];
+  const properties = [...propertiesObj["miami-dade"], ...propertiesObj.pinecrest];
 
 return (
     <section className="py-10 mt-14 bg-black">
@@ -20,7 +20,7 @@ return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
           {properties.map((property) => (
             <div key={property.id} className="group flex flex-col items-center text-center">
-              <NavLink to={`/properties/${property.location === "MIAMI-DADE" ? "miami-dade/miamiDade" : "pinecrest/pinecrest"}/${property.id}`}>
+              <NavLink to={`/properties/${property.location === "miami-dade" ? "miami-dade/miami-dade" : "pinecrest/pinecrest"}/${property.id}`}>
               <img
                 src={property.images[0]}
                 alt={property.address}
@@ -31,7 +31,7 @@ return (
                 {property.address.split(',')[0].trim()}
               </h3>
               <button
-                onClick={()=> navigate(`/properties/${property.location === "MIAMI-DADE" ? "miami-dade/miamiDade" : "pinecrest/pinecrest"}/${property.id}`)}
+                onClick={()=> navigate(`/properties/${property.location === "miami-dade" ? "miami-dade/miami-dade" : "pinecrest/pinecrest"}/${property.id}`)}
                 className="border border-white text-white px-8 py-4 mt-4 text-sm font-medium tracking-wide hover:text-black transition-all duration-300 ease-in-out
                 hover:bg-gray-200 hover:shadow-[0_8px_30px_rgba(255,255,255,0.2)]
                 hover:scale-105 hover:cursor-pointer"
