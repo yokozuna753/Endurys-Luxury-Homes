5f8152d561ba30f2a9d39a1408d6454e649ec88c
yokozuna753 <brian894526@gmail.com>
2025-09-30 10:51:48 -0400

    fixed mobile styling for property detail page

--- frontend/src/components/Properties/PropertyDetail/Description.jsx
+++ frontend/src/components/Properties/PropertyDetail/Description.jsx
@@ -5,53 +5,53 @@ export default function Description({ property }) {
   return (
     <>
       {" "}
-      <section className="">
+      <section className=" w-full flex items-center justify-center overflow-x-hidden">
         {/* Property => heading, attributes, description ==== MARGIN */}
         <div className="mx-auto">
           {/* Property => heading, attributes, description ==== CONTAINER */}
-          <div className="flex flex-col items-center justify-center max-w-7xl mx-auto lg:px-8">
+          <div className="flex flex-col items-center  max-w-7xl mx-auto lg:px-8">
             {" "}
-            <h2 className="text-4xl md:text-5xl font-light text-white tracking-wider mb-8">
+            <h2 className="text-2xl md:text-4xl md:text-5xl font-light text-white tracking-wider mb-8">
               {" "}
               Enter Property Main Heading
             </h2>{" "}
-            <div className="flex justify-center gap-16 w-full mb-8 mt-8">
+            <div className="flex justify-between md:justify-center md:gap-16 w-full mb-8 mt-8">
               {/* Stat 1 */}
               <div className="flex flex-col items-center">
-                <div className="text-3xl font-light text-white mb-2">
+                <div className="md:text-3xl font-light text-white mb-2">
                   {property.sellPrice}{" "}
                 </div>
-                <div className="text-sm tracking-widest text-gray-300 uppercase">
+                <div className="text-xs md:text-sm tracking-widest text-gray-300 uppercase">
                   {property.status}
                 </div>
               </div>
 
               {/* Stat 2 */}
               <div className="flex flex-col items-center">
-                <div className="text-3xl font-light text-white mb-2">
+                <div className="md:text-3xl font-light text-white mb-2">
                   {property.beds}
                 </div>
-                <div className="text-sm tracking-widest text-gray-300 uppercase">
+                <div className="text-xs md:text-sm tracking-widest text-gray-300 uppercase">
                   Beds
                 </div>
               </div>
 
               {/* Stat 3 */}
               <div className="flex flex-col items-center">
-                <div className="text-3xl font-light text-white mb-2">
+                <div className="md:text-3xl font-light text-white mb-2">
                   {property.baths}
                 </div>
-                <div className="text-sm tracking-widest text-gray-300 uppercase">
+                <div className="text-xs md:text-sm tracking-widest text-gray-300 uppercase">
                   Baths
                 </div>
               </div>
 
               {/* Stat 4 */}
               <div className="flex flex-col items-center">
-                <div className="text-3xl font-light text-white mb-2">
+                <div className="md:text-3xl font-light text-white mb-2">
                   {property.livingArea}
                 </div>
-                <div className="text-sm tracking-widest text-gray-300 uppercase">
+                <div className="text-xs md:text-sm tracking-widest text-gray-300 uppercase">
                   Living Size
                 </div>
               </div>
