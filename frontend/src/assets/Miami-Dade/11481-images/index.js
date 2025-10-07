889493abb831467eb11dd03a7feee26d9a78fce8
yokozuna753 <brian894526@gmail.com>
2025-10-07 10:39:33 -0400

    imported property images to array

---
+++ frontend/src/assets/Miami-Dade/11481-images/index.js
@@ -0,0 +1,57 @@
+import pool11481 from "./pool.webp";
+import poolNight11481 from "./pool-night.webp";
+import poolDay11481 from "./pool-day.webp";
+import backPool11481 from "./back-pool.webp";
+import outdoorKitchen11481 from "./outdoor-kitchen.webp";
+import driveway11481 from "./driveway.webp";
+import drivewayFront11481 from "./driveway-front.webp";
+import bathroomShower11481 from "./bathroom-shower.webp";
+import bathroomBlack11481 from "./bathroom-black.webp";
+import roomPool11481 from "./room-pool.webp";
+import indoorPool11481 from "./indoor-pool.webp";
+import roomWhite11481 from "./room-white.webp";
+import bathroomWhite2_11481 from "./bathroom-white-2.webp";
+import frontGrass11481 from "./front-grass.webp";
+import living2_11481 from "./living2.webp";
+import living11481 from "./living.webp";
+import kitchen4_11481 from "./kitchen4.webp";
+import window11481 from "./window.webp";
+import bathroomWhite11481 from "./bathroom-white.webp";
+import bathroomBlue11481 from "./bathroom-blue.webp";
+import kitchen3_11481 from "./kitchen3.webp";
+import kitchen2_11481 from "./kitchen2.webp";
+import kitchen11481 from "./kitchen.webp";
+import kitchenPool11481 from "./kitchen-pool.webp";
+import aerialTop11481 from "./aerial-top.webp";
+import aerialFront11481 from "./aerial-front.webp";
+import sideLeft11481 from "./side-left.webp";
+
+export {
+  pool11481,
+  poolNight11481,
+  poolDay11481,
+  backPool11481,
+  outdoorKitchen11481,
+  driveway11481,
+  drivewayFront11481,
+  bathroomShower11481,
+  bathroomBlack11481,
+  roomPool11481,
+  indoorPool11481,
+  roomWhite11481,
+  bathroomWhite2_11481,
+  frontGrass11481,
+  living2_11481,
+  living11481,
+  kitchen4_11481,
+  window11481,
+  bathroomWhite11481,
+  bathroomBlue11481,
+  kitchen3_11481,
+  kitchen2_11481,
+  kitchen11481,
+  kitchenPool11481,
+  aerialTop11481,
+  aerialFront11481,
+  sideLeft11481,
+};
